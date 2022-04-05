2eca5ef7020242cc1ceb1cd39cac60c647810da6
iinsue <hinsu93@naver.com>
2022-04-05 16:07:12 +0900

    #7.6 Parameters

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
-      <Route path="/movie" element={<Detail />} />
+      <Route path="/movie/:id" element={<Detail />}></Route>
     </Routes>
   );
 }
--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,11 +1,11 @@
 import PropTypes from "prop-types";
 import { Link } from "react-router-dom";
 
-function Movie({ coverImg, title, summary, genres }) {
+function Movie({ id, coverImg, title, summary, genres }) {
   return (
     <div>
       <img src={coverImg} alt={title} />
-      <Link to="/movie">
+      <Link to={`/movie/${id}`}>
         <h2>{title}</h2>
       </Link>
       <p>{summary}</p>
