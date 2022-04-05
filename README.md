990073073d1bcd6061ea0870e681888bb585de27
iinsue <hinsu93@naver.com>
2022-04-05 10:02:54 +0900

    #7.2 Coin Tracker

--- README.md
+++ README.md
@@ -4,6 +4,10 @@
 #5.1 Tour of CRA
 
 
-# React 18 index.js 설정
 
+
+- React 18 index.js 설정
+### #7.2 Coin Tracker
+- 외부 API: https://api.coinpaprika.com/v1/tickers?limit=10
+- challenge: 코인 갯수 입력해서 몇 달러인지 출력해보기.
 ### npm install prop-types
--- src/App.js
+++ src/App.js
@@ -1,35 +1,28 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 function App() {
-  const [toDo, setToDo] = useState("");
-  const [toDos, setToDos] = useState([]);
-  const onChange = (event) => setToDo(event.target.value);
-  const onSubmit = (event) => {
-    event.preventDefault();
-    if (toDo === "") {
-      return;
-    }
-    setToDo("");
-    setToDos((currentArray) => [toDo, ...currentArray]);
-  };
-  console.log(toDos);
-
+  const [loading, setLoading] = useState(true);
+  const [coins, setCoins] = useState([]);
+  useEffect(() => {
+    fetch("https://api.coinpaprika.com/v1/tickers")
+      .then((response) => response.json())
+      .then((json) => setCoins(json));
+    setLoading(false);
+  }, []);
   return (
     <div>
-      <h1>My To Dos ({toDos.length})</h1>
-      <form onSubmit={onSubmit}>
-        <input
-          onChange={onChange}
-          value={toDo}
-          text="text"
-          placeholder="Write your todo..."
-        />
-        <button>Add To Do</button>
-      </form>
-      <hr />
-      {toDos.map((item, index) => (
-        <li key={index}>{item}</li>
-      ))}
+      <h1>The Coins {loading ? "" : `(${coins.length})`} </h1>
+      {loading ? (
+        <h3>Loading...</h3>
+      ) : (
+        <select>
+          {coins.map((coin) => (
+            <option key={coin.id}>
+              {coin.name} ({coin.symbol}): {coin.quotes.USD.price} USD
+            </option>
+          ))}
+        </select>
+      )}
     </div>
   );
 }
